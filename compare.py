7b3c7fcf32cb3779a88fbdbc9ed0d464041cadbc
Hugh Williams <hughw128@gmail.com>
2016-07-04 19:15:49 +0100

    Changed comparing of ints

--- compare.py
+++ compare.py
@@ -1,7 +1,7 @@
 def CompareDict(one, two):
     if str(one)==str(two):
         return 1
-    result=[]
+    result=list()
     add=result.append
     for i in set(list(one)+list(two)):
         try:
@@ -10,11 +10,10 @@ def CompareDict(one, two):
             if str(first)==str(second):
                 add(1)
             else:
-                now=[first/second, second/first]
-                if now[0]<now[1]:
-                    add(now[0])
+                if first>second:
+                    add(first/second)
                 else:
-                    add(now[1])
+                    add(second/first)
         except TypeError:
             if type(first)==dict and type(second)==dict:
                 add(CompareDict(first, second))
