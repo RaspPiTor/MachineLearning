3343564adf343185828cd2dd7a2dbffb0cb704d3
Hugh Williams <hughw128@gmail.com>
2016-07-05 20:57:51 +0100

    Slight speed improvement

--- compare.py
+++ compare.py
@@ -1,17 +1,17 @@
 __cache__=dict()
 def CompareDict(one, two):
-    allowed=[dict, int, float]
+    allowed={dict, int, float}
     diff=set(one)^set(two)
     onev=[one[a] for a in one if type(one[a]) in allowed and a not in diff]
     twov=[two[a] for a in two if type(two[a]) in allowed and a not in diff]
     if onev==twov:
         return 1
-    key='%s%s' % (str(onev), str(twov))
+    key='%s%s' % (onev, twov)
     try:
         return __cache__[key]
     except KeyError:
         pass
-    result=[0]*len(diff)
+    result=list()
     add=result.append
     for first, second in zip(onev, twov):
         try:
@@ -27,7 +27,7 @@ def CompareDict(one, two):
         except Exception as error:
             print(error, type(error))
     try:
-        result=sum(result)/len(result)
+        result=sum(result)/(len(result)+len(diff))
     except ZeroDivisionError:
         result=0
     __cache__[key]=result
